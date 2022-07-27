819a843109cc9bea127ae0a6cf36556ce9970ee7
Jacqline <jacqlinegeng@gmail.com>
2022-07-27 13:40:17 -0400

    adding middle exercise

--- eqArrays.js
+++ eqArrays.js
@@ -24,10 +24,3 @@ const eqArrays = function(array1, array2) {
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), false);
 
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log('✅✅✅ Assertion Passed:' + arr1 + ' === ' + arr2);
-  } else {
-    console.log('🛑🛑🛑 Assertion Failed:' + arr1 + ' !== ' + arr2);
-  }
-};
