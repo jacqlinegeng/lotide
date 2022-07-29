a9a20b506a3cac0ec63bc973c82653b0bbf72bd1
Jacqline <jacqlinegeng@gmail.com>
2022-07-28 22:17:53 -0400

    practicing finding the key in an object

---
+++ findKey.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('✅✅✅ Assertion Passed:' + actual + ' === ' + expected);
+  } else {
+    console.log('🛑🛑🛑 Assertion Failed:' + actual + ' !== ' + expected);
+  }
+};
+
+
+const findKey = function(object, callback) {
+  // const arrayOfObj = Object.keys(object); // this puts the object keys into an array
+  for (let key in object) { // for..in loop is used for objects
+    if (callback(object[key])) return key; // the callback argument is x.stars === 2, which gets summed into callback
+    //object[key].star === 2 <== this is not used because we want function to have reusability
+    //console.log(object[key]);
+  }
+};
+
+const check = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 6 },
+  "Akelarre":  { stars: 2 }
+}, x => x.stars === 6); // => "noma"
+
+//console.log(check)
+assertEqual(check, "Ora");
