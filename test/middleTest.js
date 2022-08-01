ebc48e7e35c3104b7dd4422b9998cd78a5bfaec4
Jacqline <jacqlinegeng@gmail.com>
2022-08-01 16:30:49 -0400

    adding middle test

---
+++ test/middleTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual');
+const middle = require('../middle');
+
+const middleNum = middle([1, 5, 26, 7, 8]);
+console.log(middleNum);
