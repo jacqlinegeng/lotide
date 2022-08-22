66d967b6f375c85354a6f335359774332879b682
Jacqline <jacqlinegeng@gmail.com>
2022-08-22 11:28:48 -0400

    refactoring head.js

--- head.js
+++ head.js
@@ -1,5 +1,15 @@
-const head = function(array) {
-  return array[0];
+const head = array => array[0];
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
 };
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
 module.exports = head;
