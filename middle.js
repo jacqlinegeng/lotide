920b1ca02b5f8b168c5d9a705a49db57eac0c6ea
Jacqline <jacqlinegeng@gmail.com>
2022-08-23 15:32:45 -0400

    refactor middle and without functions

--- middle.js
+++ middle.js
@@ -1,30 +1,22 @@
 const assertEqual = require('./assertEqual');
-
-// console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
-// console.log(assertEqual(1, 1));
-
 const eqArrays = require('./eqArrays');
-
 const assertArraysEqual = require('./assertArraysEqual');
 
-const middle = function(array) {
+const middle = (array) => {
   let middleIndex = Math.floor(array.length / 2);
+  if (array.length <= 1) {
+    return array;
+  }
+  
+
   for (let i = 0; i < array.length; i++) {
-    if (array.length > 1) {
-      if (array.length % 2 === 1) {
-        return [array[middleIndex]];
-      } else {
-        return [array[middleIndex - 1], array[middleIndex]];
-      }
-      // console.log(array[middleIndex]);
+    if (array.length % 2 === 1) {
+      return [array[middleIndex]];
+    } else {
+      return [array[middleIndex - 1], array[middleIndex]];
     }
   }
 
 };
-// [1, 5, 26, 7, 8] var middleIndex = array.length/2
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-// assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), false);
-// const output = middle([1, 5, 26, 7, 8]);
-// console.log(output);
 
 module.exports = middle;
--- without.js
+++ without.js
@@ -2,21 +2,20 @@ const assertEqual = require('./assertEqual');
 const eqArrays = require('./eqArrays');
 const assertArraysEqual = require('./assertArraysEqual');
 
-const without = function(source, itemsToRemove) {
-  //take in a source array and a itemsToRemove array
-  // return source.filter(element => !itemsToRemove.includes(element));
-  const filteredSource = [];
-  for (const element of source) {
-    if (!itemsToRemove.includes(element)) {
-      filteredSource.push(element);
-    }
-  }
+const without = (source, itemsToRemove) => {
+  
+  return source.filter(element => !itemsToRemove.includes(element));
+  
+  
+  // const filteredSource = [];
+  // for (const element of source) {
+  //   if (!itemsToRemove.includes(element)) {
+  //     filteredSource.push(element);
+  //   }
+  // }
 
-  return filteredSource;
+  // return filteredSource;
 };
-
-// assertArraysEqual([1,2,3], [1,2,3]);
-// assertArraysEqual([1,2,3], [1,2,'3']);
 
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]);
