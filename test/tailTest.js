1b4f44286d355895412f3109ec2724a58c661ca2
Jacqline <jacqlinegeng@gmail.com>
2022-08-29 10:22:29 -0700

    refactor eqArrays test

--- test/tailTest.js
+++ test/tailTest.js
@@ -4,7 +4,6 @@ const assertEqual = require('../assertEqual');
 // const assertArraysEqual = require('../assertArraysEqual');
 
 
-
 describe("#tail", () => {
   it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
     assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
